6c847e40daefcaef8dcb81658032354f3707c43a
Kractero <kractero@sideroca.com>
2023-10-14 20:10:51 -0700

    Legendary cte updater

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# gold-ledger
+# ledger
 
-A site to view data about the top 100 cards players stats, including bank, deck value and more. All data is sortable either descending or ascending.
+A site to view data about 
 
-New data is generated every midnight and previously generated ones can be searched and viewed.
+1. the top 100 cards players stats, including bank, deck value and more. All data is sortable either descending or ascending.
+2. living status of legendaries
+
+New data is generated every 1 AM PST and previously generated ones can be searched and viewed.
+
+Why 1 AM PST? I have a repo generating the data needed for the cte checker running at midnight PST based off NS nation dumps which should be made at 10:30 PST, this offset is to ensure they exist.
+
+Why doesn't that repo also just run the script after so it is guaranteed to exist?
+Great question!!!
---
+++ legendaries.js
@@ -0,0 +1,23 @@
+import { writeFileSync, readFileSync } from "fs";
+import { XMLParser } from "fast-xml-parser";
+
+const parser = new XMLParser({ ignoreAttributes: false });
+
+const legendaries = JSON.parse(readFileSync("Legendaries.json"))
+
+const currentDate = new Date();
+const utcMinus7Date = new Date(currentDate.getTime() - 7 * 60 * 60 * 1000);
+utcMinus7Date.setDate(utcMinus7Date.getDate() - 1);
+const date = utcMinus7Date.toISOString().slice(0, 10);
+const filePath = `https://raw.githubusercontent.com/Kractero/region-xml-dump/main/data/${date}-Nations.xml`;
+const data = await fetch(filePath);
+const text = await data.text();
+const doc = parser.parse(text);
+const names = doc.NATIONS.NATION.map((nation) => nation.NAME);
+legendaries.forEach((legendary) => {
+  const exists = names.includes(legendary.name);
+  if (exists) legendary.exists = true
+  else legendary.exists = false;
+});
+
+writeFileSync(`Legendaries.json`, JSON.stringify(legendaries, null, 2));
