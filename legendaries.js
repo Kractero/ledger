77773ca28747eb433f1c84cb98e7b81cfce1415a
Kractero <kractero@sideroca.com>
2023-10-14 21:31:32 -0700

    most confusing sortable table ever

--- legendaries.js
+++ legendaries.js
@@ -13,9 +13,9 @@ const filePath = `https://raw.githubusercontent.com/Kractero/region-xml-dump/mai
 const data = await fetch(filePath);
 const text = await data.text();
 const doc = parser.parse(text);
-const names = doc.NATIONS.NATION.map((nation) => nation.NAME);
+const names = doc.NATIONS.NATION.map((nation) => nation.NAME.toString().toLowerCase());
 legendaries.forEach((legendary) => {
-  const exists = names.includes(legendary.name);
+  const exists = names.includes(legendary.name.toLowerCase());
   if (exists) legendary.exists = true
   else legendary.exists = false;
 });
