7401e1f94c5f3a28f57174ae88e2e9d689364cd3
Kractero <115837402+Kractero@users.noreply.github.com>
2023-10-20 13:14:47 -0700

    changelog for ctes

--- legendaries.js
+++ legendaries.js
@@ -4,6 +4,7 @@ import { XMLParser } from "fast-xml-parser";
 const parser = new XMLParser({ ignoreAttributes: false });
 
 const legendaries = JSON.parse(readFileSync("Legendaries.json"))
+const legendaries_data = JSON.parse(readFileSync("LegendariesData.json"))
 
 const currentDate = new Date();
 const utcMinus7Date = new Date(currentDate.getTime() - 7 * 60 * 60 * 1000);
@@ -14,10 +15,24 @@ const data = await fetch(filePath);
 const text = await data.text();
 const doc = parser.parse(text);
 const names = doc.NATIONS.NATION.map((nation) => nation.NAME.toString().toLowerCase());
+
+const legdata = {
+  date: date,
+  changes: []
+}
 legendaries.forEach((legendary) => {
   const exists = names.includes(legendary.name.toLowerCase());
-  if (exists) legendary.exists = true
-  else legendary.exists = false;
+  if (exists !== legendary.exists) {
+    legdata.changes.push({
+      "name": legendary.name,
+      "seasons": [legendary.S1, legendary.S2, legendary.S3],
+      "old": legendary.exists,
+      "new": exists
+    })
+  }
+  legendary.exists = exists;
 });
+legendaries_data.unshift(legdata)
 
-writeFileSync(`Legendaries-${date}.json`, JSON.stringify(legendaries, null, 2));
+writeFileSync(`Legendaries.json`, JSON.stringify(legendaries, null, 2));
+writeFileSync(`LegendariesData.json`, JSON.stringify(legendaries_data, null, 2));
