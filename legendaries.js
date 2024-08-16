08b6b0c52b2a28572d051832c3e1dded4df3acd7
Kractero <kractero@sideroca.com>
2024-08-15 19:20:55 -0700

    Pull cte from region-xml-dump cte json

--- legendaries.js
+++ legendaries.js
@@ -1,39 +1,34 @@
-import { writeFileSync, readFileSync } from "fs";
-import { XMLParser } from "fast-xml-parser";
+import { writeFileSync, readFileSync } from 'fs';
 
-const parser = new XMLParser({ ignoreAttributes: false });
-
-const legendaries = JSON.parse(readFileSync("Legendaries.json"))
-const legendaries_data = JSON.parse(readFileSync("LegendariesChangelog.json"))
+const legendaries = JSON.parse(readFileSync('Legendaries.json'));
+const legendaries_data = JSON.parse(readFileSync('LegendariesChangelog.json'));
 
 const currentDate = new Date();
 const utcMinus7Date = new Date(currentDate.getTime() - 7 * 60 * 60 * 1000);
 utcMinus7Date.setDate(utcMinus7Date.getDate() - 1);
 const date = utcMinus7Date.toISOString().slice(0, 10);
-const filePath = `https://raw.githubusercontent.com/Kractero/region-xml-dump/main/data/${date}-Nations.xml`;
+const filePath = `https://raw.githubusercontent.com/Kractero/region-xml-dump/main/data/${date}-cards.json`;
 const data = await fetch(filePath);
-const text = await data.text();
-const doc = parser.parse(text);
-const names = doc.NATIONS.NATION.map((nation) => nation.NAME.toString().toLowerCase());
+const json = await data.json();
 
 const legdata = {
   date: date,
-  changes: []
-}
-legendaries.forEach((legendary) => {
-  const exists = names.includes(legendary.name.toLowerCase());
-  if (exists !== legendary.exists) {
+  changes: [],
+};
+legendaries.forEach(legendary => {
+  const cte = json[legendary.name];
+  if (!cte !== legendary.exists) {
     legdata.changes.push({
-      "name": legendary.name,
-      "seasons": [legendary.S1, legendary.S2, legendary.S3],
-      "old": legendary.exists,
-      "new": exists
-    })
+      name: legendary.name,
+      seasons: [legendary.S1, legendary.S2, legendary.S3],
+      old: legendary.exists,
+      new: !cte,
+    });
   }
-  legendary.exists = exists;
+  legendary.exists = !cte;
 });
 
-if (legdata.changes.length > 0) legendaries_data.unshift(legdata)
+if (legdata.changes.length > 0) legendaries_data.unshift(legdata);
 
 writeFileSync(`Legendaries.json`, JSON.stringify(legendaries, null, 2));
 writeFileSync(`LegendariesChangelog.json`, JSON.stringify(legendaries_data, null, 2));
